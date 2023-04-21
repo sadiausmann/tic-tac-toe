2d07b4ba4a2dd3d8bab5472794761afae2e06f4d
sadia <sadia_43@hotmail.com>
2023-04-21 15:22:40 +0930

    winning conditions witten

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
-var box = document.querySelector('.box');
 var boxes = document.querySelectorAll("div");
 var gameBoard = document.querySelector("section.gameBoard");
 var whosTurn = "X";
 
+var box1= document.querySelector('.box1')
+var box2= document.querySelector('.box2')
+var box3= document.querySelector('.box3')
+var box4= document.querySelector('.box4')
+var box5= document.querySelector('.box5')
+var box6= document.querySelector('.box6')
+var box7= document.querySelector('.box7')
+var box8= document.querySelector('.box8')
+var box9= document.querySelector('.box9')
 
 gameBoard.addEventListener("click", function (event) {
     var box = event.target;
     if (box.textContent === "" && whosTurn === "X"){
         box.textContent = "X";
-            whosTurn = "O"
+        whosTurn = "O";
+        if (box1.textContent === "X" && box2.textContent === "X" && box3.textContent === "X"){
+            console.log("Player X won the game!")
+        }else if (box4.textContent === "X" && box5.textContent === "X" && box6.textContent === "X"){
+            console.log("Player X won the game!")
+        }else if (box7.textContent === "X" && box8.textContent === "X" && box9.textContent === "X"){
+            console.log("Player X won the game!")
+        }else if (box1.textContent === "X" && box4.textContent === "X" && box7.textContent === "X"){
+            console.log("Player X won the game!")
+        }else if (box2.textContent === "X" && box5.textContent === "X" && box8.textContent === "X"){
+            console.log("Player X won the game!")
+        }else if (box3.textContent === "X" && box6.textContent === "X" && box9.textContent === "X"){
+            console.log("Player X won the game!")
+        }else if (box1.textContent === "X" && box5.textContent === "X" && box9.textContent === "X"){
+            console.log("Player X won the game!")
+        }else if (box3.textContent === "X" && box5.textContent === "X" && box7.textContent === "X"){
+            console.log("Player X won the game!")
+        }
     }else {
             box.textContent = "O";
-            whosTurn = "X"
-        }
+            whosTurn = "X";
+            if (box1.textContent === "O" && box2.textContent === "O" && box3.textContent === "O"){
+                console.log("Player O won the game!")
+            }else if (box4.textContent === "O" && box5.textContent === "O" && box6.textContent === "O"){
+                console.log("Player O won the game!")
+            }else if (box7.textContent === "O" && box8.textContent === "O" && box9.textContent === "O"){
+                console.log("Player O won the game!")
+            }else if (box1.textContent === "O" && box4.textContent === "O" && box7.textContent === "O"){
+                console.log("Player O won the game!")
+            }else if (box2.textContent === "O" && box5.textContent === "O" && box8.textContent === "O"){
+                console.log("Player O won the game!")
+            }else if (box3.textContent === "O" && box6.textContent === "O" && box9.textContent === "O"){
+                console.log("Player O won the game!")
+            }else if (box1.textContent === "O" && box5.textContent === "O" && box9.textContent === "O"){
+                console.log("Player O won the game!")
+            }else if (box3.textContent === "O" && box5.textContent === "O" && box7.textContent === "O"){
+                console.log("Player O won the game!")
+            }
+        }     
+    
 });
 
