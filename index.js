26ff8ec1ec18af95e3caca8da608675fdf25342b
sadia <sadia_43@hotmail.com>
2023-04-21 17:03:20 +0930

    added conditions for  game draw

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 var boxes = document.querySelectorAll("div");
+var box = document.querySelector("div");
 var gameBoard = document.querySelector("section.gameBoard");
 var displayResult = document.querySelector("h2");
 var whosTurn = "X";
 
-var box1= document.querySelector('.box1')
-var box2= document.querySelector('.box2')
-var box3= document.querySelector('.box3')
-var box4= document.querySelector('.box4')
-var box5= document.querySelector('.box5')
-var box6= document.querySelector('.box6')
-var box7= document.querySelector('.box7')
-var box8= document.querySelector('.box8')
-var box9= document.querySelector('.box9')
+var box1= document.querySelector('.box1');
+var box2= document.querySelector('.box2');
+var box3= document.querySelector('.box3');
+var box4= document.querySelector('.box4');
+var box5= document.querySelector('.box5');
+var box6= document.querySelector('.box6');
+var box7= document.querySelector('.box7');
+var box8= document.querySelector('.box8');
+var box9= document.querySelector('.box9');
 
 gameBoard.addEventListener("click", function (event) {
     var box = event.target;
     if (box.textContent === "" && whosTurn === "X"){
-        box.textContent = "X"
+        box.textContent = "X";
         whosTurn = "O"
         if (box1.textContent === "X" && box2.textContent === "X" && box3.textContent === "X"){
             displayResult.textContent = "Player X won the game!";
@@ -34,9 +35,9 @@ gameBoard.addEventListener("click", function (event) {
             displayResult.textContent = "Player X won the game!";
         }else if (box3.textContent === "X" && box5.textContent === "X" && box7.textContent === "X"){
             displayResult.textContent = "Player X won the game!";
-        }
+        };
     }else{
-        box.textContent = "O"
+        box.textContent = "O";
         whosTurn = "X"
         if (box1.textContent === "O" && box2.textContent === "O" && box3.textContent === "O"){
             displayResult.textContent = "Player O won the game!";
@@ -53,9 +54,11 @@ gameBoard.addEventListener("click", function (event) {
         }else if (box1.textContent === "O" && box5.textContent === "O" && box9.textContent === "O"){
             displayResult.textContent = "Player O won the game!";
         }else if (box3.textContent === "O" && box5.textContent === "O" && box7.textContent === "O"){ 
-            displayResult.textContent = "Player O won the game!";
-            
+            displayResult.textContent = "Player O won the game!";    
+        }    
     }   
-    
-}     
+    if (box1.textContent !== "" && box2.textContent !== "" && box3.textContent !== "" && box4.textContent !== "" && box5.textContent !== "" && box6.textContent !== "" && box7.textContent !== "" && box8.textContent !== "" && box9.textContent !== "" ){
+        displayResult.textContent = "Game Draw!";  
+    }
 });
+
