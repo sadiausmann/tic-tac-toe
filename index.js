0033977bd539cfd899745d8114b645dd44629675
sadia <sadia_43@hotmail.com>
2023-04-22 17:36:05 +0930

    added condition to other turn and applied DRY to some extend

--- index.js
+++ index.js
@@ -4,61 +4,45 @@ var gameBoard = document.querySelector("section.gameBoard");
 var displayResult = document.querySelector("h2");
 var whosTurn = "X";
 
-var box1= document.querySelector('.box1');
-var box2= document.querySelector('.box2');
-var box3= document.querySelector('.box3');
-var box4= document.querySelector('.box4');
-var box5= document.querySelector('.box5');
-var box6= document.querySelector('.box6');
-var box7= document.querySelector('.box7');
-var box8= document.querySelector('.box8');
-var box9= document.querySelector('.box9');
+
 
 gameBoard.addEventListener("click", function (event) {
     var box = event.target;
     if (box.textContent === "" && whosTurn === "X"){
         box.textContent = "X";
         whosTurn = "O"
-        if (box1.textContent === "X" && box2.textContent === "X" && box3.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
-        }else if (box4.textContent === "X" && box5.textContent === "X" && box6.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
-        }else if (box7.textContent === "X" && box8.textContent === "X" && box9.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
-        }else if (box1.textContent === "X" && box4.textContent === "X" && box7.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
-        }else if (box2.textContent === "X" && box5.textContent === "X" && box8.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
-        }else if (box3.textContent === "X" && box6.textContent === "X" && box9.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
-        }else if (box1.textContent === "X" && box5.textContent === "X" && box9.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
-        }else if (box3.textContent === "X" && box5.textContent === "X" && box7.textContent === "X"){
-            displayResult.textContent = "Player X won the game!";
+        if ((boxes[0].textContent === "X" && boxes[1].textContent === "X" && boxes[2].textContent === "X") ||
+        (boxes[3].textContent === "X" && boxes[4].textContent === "X" && boxes[5].textContent === "X") ||
+        (boxes[6].textContent === "X" && boxes[7].textContent === "X" && boxes[8].textContent === "X") ||
+        (boxes[0].textContent === "X" && boxes[3].textContent === "X" && boxes[6].textContent === "X") ||
+        (boxes[1].textContent === "X" && boxes[4].textContent === "X" && boxes[7].textContent === "X") || 
+        (boxes[2].textContent === "X" && boxes[5].textContent === "X" && boxes[8].textContent === "X") ||  
+        (boxes[0].textContent === "X" && boxes[4].textContent === "X" && boxes[8].textContent === "X") ||
+        (boxes[2].textContent === "X" && boxes[4].textContent === "X" && boxes[6].textContent === "X")){
+            displayResult.textContent = "Player X won the game!"
         };
-    }else{
+    }else if (box.textContent !== "X" && whosTurn === "O"){
         box.textContent = "O";
         whosTurn = "X"
-        if (box1.textContent === "O" && box2.textContent === "O" && box3.textContent === "O"){
-            displayResult.textContent = "Player O won the game!";
-        }else if (box4.textContent === "O" && box5.textContent === "O" && box6.textContent === "O"){
-            displayResult.textContent = "Player O won the game!";
-        }else if (box7.textContent === "O" && box8.textContent === "O" && box9.textContent === "O"){
-            displayResult.textContent = "Player O won the game!";
-        }else if (box1.textContent === "O" && box4.textContent === "O" && box7.textContent === "O"){
-            displayResult.textContent = "Player O won the game!";
-        }else if (box2.textContent === "O" && box5.textContent === "O" && box8.textContent === "O"){
-            displayResult.textContent = "Player O won the game!";
-        }else if (box3.textContent === "O" && box6.textContent === "O" && box9.textContent === "O"){
-            displayResult.textContent = "Player O won the game!";
-        }else if (box1.textContent === "O" && box5.textContent === "O" && box9.textContent === "O"){
-            displayResult.textContent = "Player O won the game!";
-        }else if (box3.textContent === "O" && box5.textContent === "O" && box7.textContent === "O"){ 
-            displayResult.textContent = "Player O won the game!";    
-        }    
-    }   
-    if (box1.textContent !== "" && box2.textContent !== "" && box3.textContent !== "" && box4.textContent !== "" && box5.textContent !== "" && box6.textContent !== "" && box7.textContent !== "" && box8.textContent !== "" && box9.textContent !== "" ){
-        displayResult.textContent = "Game Draw!";  
+        if ((boxes[0].textContent === "O" && boxes[1].textContent === "O" && boxes[2].textContent === "O") ||
+        (boxes[3].textContent === "O" && boxes[4].textContent === "O" && boxes[5].textContent === "O") ||
+        (boxes[6].textContent === "O" && boxes[7].textContent === "O" && boxes[8].textContent === "O") ||
+        (boxes[0].textContent === "O" && boxes[3].textContent === "O" && boxes[6].textContent === "O") ||
+        (boxes[1].textContent === "O" && boxes[4].textContent === "O" && boxes[7].textContent === "O") ||
+        (boxes[2].textContent === "O" && boxes[5].textContent === "O" && boxes[8].textContent === "O") ||
+        (boxes[0].textContent === "O" && boxes[4].textContent === "O" && boxes[8].textContent === "O") ||
+        (boxes[2].textContent === "O" && boxes[4].textContent === "O" && boxes[6].textContent === "O")){ 
+            displayResult.textContent = "Player O won the game!"
+        }
+    }else if (boxes[0].textContent !== "" && 
+        boxes[1].textContent !== "" && 
+        boxes[2].textContent !== "" && 
+        boxes[3].textContent !== "" && 
+        boxes[4].textContent !== "" && 
+        boxes[5].textContent !== "" && 
+        boxes[6].textContent !== "" && 
+        boxes[7].textContent !== "" && 
+        boxes[8].textContent !== "" ){
+            displayResult.textContent = "Game Draw!"
     }
 });
-
